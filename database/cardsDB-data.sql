daa9932a3c73f3fdaf8bed60fa22db2dc3f2b88f
michael collins <mcollins9915@gmail.com>
2022-05-18 14:44:54 -0500

    first commit

---
+++ database/cardsDB-data.sql
@@ -0,0 +1,14 @@
+
+
+INSERT INTO card
+(fName, lName, team, manager, price, image)
+VALUES ('Alexandre', 'Lacazette', 'Arsenal', 'Mikel Arteta', 5, 'alexandere.jpg'),
+('Ollie', 'Watkins', 'Aston Villa', 'Steven Gerrard', 2, 'ollie.jpg'),
+('Sergi', 'Canos', 'Brentford', 'Thomas Frank', 2, 'sergi.jpg'),
+('Danny', 'Welbeck', 'Brighton', 'Graham Potter', 1, 'danny.jpg'),
+('Ashley', 'Barnes', 'Burnley', 'Sean Dyche', 1,'ashley.jpg'),
+('Christian', 'Pulisic', 'Chelsea', 'Thomas Tuchel', 5, 'christan.jpg'),
+('Wilfried', 'Zaha', 'Crystal Palace', 'Patrick Vieira', 4, 'wilfried.jpg'),
+('Dominic', 'Calvert-Lewin', 'Everton', 'Frank Lampard', 4, 'dominic.jpg'),
+('Patrick', 'Bamford', 'Leeds United', 'Marcelo Bielsa', 3, 'patrick.jpg'),
+('Jamie', 'Vardy', 'Leicester City', 'Brendan Rodgers', 5, 'jamie.jpg');
